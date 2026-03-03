abbe7c808570088b78e4c88eaced1f3f47c115cb
Toolbox <danielalejandro.leivafunes9@gmail.com>
2026-03-02 20:29:32 -0600

    Very quickly changed the command imports so that the statement isn't comically large anymore

--- src/index.ts
+++ src/index.ts
@@ -4,21 +4,21 @@
 import type { CommandsRegistry } from "./commands";
 
 import { setUser, readConfig } from "./config";
-import { handlerLogin, handlerRegisterUser, handlerGetUsers, handlerDeleteAllUsers, handlerAddFeed, handlerGetFeeds, handlerFollow, handlerGetFollows, handlerAggregate, registerCommand, middlewareLoggedIn, runCommand } from "./commands";
+import * as commands from "./commands";
 
 async function main() {
     const registry: CommandsRegistry = {};
 
     // Keep an eye out on the function signatures. Some of them will need to be wrapped in middleware to function properly.
-    registerCommand(registry, "login", handlerLogin);
-    registerCommand(registry, "register", handlerRegisterUser);
-    registerCommand(registry, "users", handlerGetUsers);
-    registerCommand(registry, "reset", handlerDeleteAllUsers);
-    registerCommand(registry, "addfeed", middlewareLoggedIn(handlerAddFeed));
-    registerCommand(registry, "feeds", handlerGetFeeds);
-    registerCommand(registry, "follow", middlewareLoggedIn(handlerFollow));
-    registerCommand(registry, "following", middlewareLoggedIn(handlerGetFollows));
-    registerCommand(registry, "agg", handlerAggregate);
+    commands.registerCommand(registry, "login", commands.handlerLogin);
+    commands.registerCommand(registry, "register", commands.handlerRegisterUser);
+    commands.registerCommand(registry, "users", commands.handlerGetUsers);
+    commands.registerCommand(registry, "reset", commands.handlerDeleteAllUsers);
+    commands.registerCommand(registry, "addfeed", commands.middlewareLoggedIn(commands.handlerAddFeed));
+    commands.registerCommand(registry, "feeds", commands.handlerGetFeeds);
+    commands.registerCommand(registry, "follow", commands.middlewareLoggedIn(commands.handlerFollow));
+    commands.registerCommand(registry, "following", commands.middlewareLoggedIn(commands.handlerGetFollows));
+    commands.registerCommand(registry, "agg", commands.handlerAggregate);
     const userArgs = process.argv.slice(2);
     if (userArgs.length === 0) {
         console.error("Please provide a command.");
@@ -27,7 +27,7 @@ async function main() {
 
     const cmdName = userArgs[0];
     const args = userArgs.slice(1);
-    await runCommand(registry, cmdName, ...args);
+    await commands.runCommand(registry, cmdName, ...args);
     process.exit(0);
 }
 
